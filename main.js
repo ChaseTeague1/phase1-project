08d2bdab06f49f356b4ea726b6fe8cd5604781b3
ChaseTeague <teaguechase@yahoo.com>
2023-12-12 12:23:56 -0500

    adding basic steps for project

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ function loadOffers(){
         console.log(offer)
     }))
 }
+
+/*
+assign the form element to a variable.
+create a function to fetch offers.
+on page load display created offers.
+iterate through every offer and assign each of them a card to be displayed.
+create an offer button for each card.
+create a comment section for each card.
+create an add offer button that will allow user to post an offer
+create a light mode and dark mode button.
+*/
