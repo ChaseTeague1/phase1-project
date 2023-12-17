78a4791d12165e830b7c0c19fdf8a3be58355851
ChaseTeague <teaguechase@yahoo.com>
2023-12-17 05:25:37 -0500

    adding post request allowing user to post own listing

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 let cardContainer = document.getElementById('card-container');
 
+//grab form element and assign it to a variable
+const form = document.querySelector(".add-post-form");
+
+//grab submit button
+const addListing = document.querySelector('.submit');
+
 //*****DISPLAY PREVIOUS OFFERS FROM DB.JSON ON PAGE LOAD******//
 //create an event listener to listen for page load.
-document.addEventListener('DOMContentLoaded', loadOffers)
+document.addEventListener('DOMContentLoaded', () => {
+    //create on submit event listener for form
+    form.addEventListener('submit', (e) => {
+        console.log('I am working')
+        e.preventDefault();
+        createOffer(e.target);
+        form.reset();
+    })
+    loadOffers();
+})
 
 //make a callback function to pass event listener
 function loadOffers(){
@@ -24,9 +39,8 @@ function offerInfo(offer){
     card.innerHTML = `
     <div class="card">
         <h1 id="item-name">${offer.name}</h1>
-        <p id="user-name">By: Chase Teague</p>
         <img class="image" src="${offer.image}">
-        <h4 id="item-price">${offer.price}</h4>
+        <h4 id="item-price">$${offer.price}</h4>
         <p id="description">${offer.description}</p>
         <button class ="offer-button" id="${offer.id}">Make Offer</button>
     </div>
@@ -36,13 +50,29 @@ function offerInfo(offer){
 }
 
 //*****ADD LISTING BUTTON VIA POST REQUEST********//
-//grab form element and assign to variable
-const form = document.querySelector(".add-post-form");
-//grab create listing button
-const addListing = document.querySelector('.submit');
-//create on submit event listener for form
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
 //create callback function that holds POST request
-//create a newOffer element that will be appended to cardContainer on submit
+function createOffer(){
+    let name = document.getElementById('name');
+    let image = document.getElementById('image');
+    let description = document.getElementById('description');
+    let price = document.getElementById('price');
+    fetch('http://localhost:3000/offers', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            Accept : 'application/json',
+        },
+        body : JSON.stringify({
+            'name' : name.value,
+            'image': image.value,
+            'description': description.value,
+            'price': price.value,
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        //create a newOffer element that will be appended to cardContainer on submit
+        let newOffer = offerInfo(data);
+        cardContainer.append(newOffer)
+    })
+}
